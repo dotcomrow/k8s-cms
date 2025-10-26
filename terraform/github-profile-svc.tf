aa7f69a7c808d83d904b1c66dd5393ee8c5666b2
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-10-26 16:41:50 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/github-profile-svc.tf
+++ terraform/github-profile-svc.tf
@@ -137,7 +137,6 @@ resource "google_cloud_run_v2_service" "github_profile_svc" {
     google_project_iam_member.registry_permissions,
     google_project_iam_member.secret_manager_grant,
     null_resource.ghcr_to_gcp_image_sync,
-    google_project_iam_member.eventarc_receive_auditlog,
     null_resource.kms_iam_binding,
     google_project_iam_member.cloud_run_secret_list
   ]
